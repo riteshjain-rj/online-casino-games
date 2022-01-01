2e02847004e73042cbc8525de2eba5f6d18405bf
Ritesh Jain <riteshjain@gmail.com>
2022-01-01 18:04:32 +0000

    Initial commit

---
+++ src/main/java/com/online/casino/games/OnlineCasinoGamesApplication.java
@@ -0,0 +1,13 @@
+package com.online.casino.games;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineCasinoGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineCasinoGamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/online/casino/games/OnlineCasinoGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.online.casino.games;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCasinoGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
